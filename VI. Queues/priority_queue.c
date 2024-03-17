1618d982555c61636f0b3d1887b1889b7408090f
azatshtru <adeveloper121@gmail.com>
2024-03-17 19:15:30 +0530

    Implemented Priority Queues

--- VI. Queues/priority_queue.c
+++ VI. Queues/priority_queue.c
@@ -14,7 +14,35 @@ PriorityQueue* createPriorityQueue() {
     return q;
 }
 
-int nq(PriorityQueue* q) {
-    
+int swap(float* a, float* b) {
+    float temp = *a;
+    *a = *b;
+    *b = temp;
     return 0;
+}
+
+int nq(PriorityQueue* q, float x) {
+    push(q->A, x);
+    int i = q->A->fill;
+    while(x >= *(q->A->array+(i-1)/2) && i > 0) {
+        *(q->A->array+i) = *(q->A->array+(i-1)/2);
+        *(q->A->array+(i-1)/2) = x;
+        i = (i-1)/2;
+    }
+    return 0;
+}
+
+float dq(PriorityQueue* q) {
+    swap(q->A->array, q->A->array + q->A->fill);
+    float v = pop(q->A, q->A->fill);
+    
+    int i = 0;
+    while(2*i+1 <= q->A->fill) {
+        int max_index;
+        if(2*i+2 > q->A->fill) { max_index = 2*i+1; }
+        else { max_index = *(q->A->array + 2*i+1) > *(q->A->array + 2*i+2) ? 2*i+1 : 2*i+2; }
+        if(*(q->A->array + max_index) > *(q->A->array + i)) { swap(q->A->array + max_index, q->A->array + i); }
+        i = max_index;
+    }
+    return v;
 }
