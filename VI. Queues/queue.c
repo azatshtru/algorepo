78eb59a4f974008642294c7c2f993258774b6657
azatshtru <adeveloper121@gmail.com>
2024-03-17 17:38:56 +0530

    Implemented Queues using Linked list

--- VI. Queues/queue.c
+++ VI. Queues/queue.c
@@ -2,7 +2,28 @@
 
 typedef struct queue {
     LinkedList* A;
-    Node* front;
-    Node* back;
 } Queue;
 
+Queue* createQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->A = createLinkedList();
+    return q;
+}
+
+int nq(Queue* q, float x) {
+    return push(q->A, x);
+}
+
+float dq(Queue* q) {
+    Node* temp = q->A->head;
+    q->A->head = q->A->head->next;
+    float v = temp->value;
+    free(temp);
+    q->A->size -= 1;
+    return v;
+}
+
+int displayQueue(Queue* q) {
+    prettyPrint(q->A);
+    return 0;
+}
