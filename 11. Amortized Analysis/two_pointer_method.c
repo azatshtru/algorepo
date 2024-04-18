b9b42636033b23866fcb3ac77afa2d96624fb32c
azatshtru <adeveloper121@gmail.com>
2024-04-18 19:11:41 +0530

    Added two pointer method

---
+++ 11. Amortized Analysis/two_pointer_method.c
@@ -0,0 +1,36 @@
+#include "../II. Sorting/sorting.c"
+
+//Find if a subarray sums to a number k.
+int find_subarray_sum(int len, int* A, int k) {
+    int p = 0;
+    int q = 0;
+    int sum = A[0];
+    while(q != len-1 || p != len-1) {
+        if(sum==k) {
+            printf("%d:%d\n", p, q);
+            return 1;
+        }
+        if(sum < k && q+1<len && sum+A[q+1] <= k) { 
+            q += 1;
+            sum += A[q];
+        } else if(q+1<len && sum+A[q+1] > k && p+1 <= q) {
+            sum -= A[p];
+            p += 1;
+        }
+    }
+    return -1;
+}
+
+//Find two numbers in the array such that their sum is k.
+int two_sum(int len, float* A, int k) {
+    //sort the array
+    merge_sort(A, -1, len-1);
+    int p = 0;
+    int q = len-1;
+    while(p != q) {
+        if((int)(A[p]+A[q]) == k) { printf("\n%d+%d=%d\n", (int)A[p], (int)A[q], (int)(A[p]+A[q])); return 1; }
+        if((int)(A[p]+A[q]) < k) { p+=1; }
+        else if((int)(A[p]+A[q]) > k) { q-=1; }
+    }
+    return -1;
+}
