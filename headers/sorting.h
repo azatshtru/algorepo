407cf1abe89e5459fe68f0a1ab8a1c2ed1b16457
azatshtru <adeveloper121@gmail.com>
2024-08-28 13:51:10 +0530

    Added hashsets

--- headers/sorting.h
+++ headers/sorting.h
@@ -11,6 +11,7 @@ void insertion_sort(void* array, uint32 typesize, uint32 length, float(*cmp_fn)(
 void merge_sort(void* array, uint32 typesize, uint32 start, uint32 end, float(*cmp_fn)(void*, void*));
 void quicksort(void* array, int typesize, int lo, int hi, float(*cmp_fn)(void*, void*));
 void counting_sort(void* array, uint32 typesize, uint32 length, float(*cmp_fn)(void*, void*));
+void radix_sort(void* array, uint32 typesize, uint32 length, float(*cmp_fn)(void*, void*));
 void bucket_sort(void* array, uint32 typesize, uint32 length, float(*cmp_fn)(void*, void*));
 void bogosort(void* array, uint32 typesize, uint32 length, float(*cmp_fn)(void*, void*));
 
--- main.c
+++ main.c
@@ -3,26 +3,8 @@
 #include "headers/binary_search.h"
 #include "common_collections/include/vector.h"
 
-float cmp_float(void* a, void* b) {
-    return (*((float*)a)) - (*((float*)b));
-}
 
 int main() {
-    float numbers[9];
-    numbers[0] = -1.3;
-    numbers[1] = 2.1;
-    numbers[2] = 4.12;
-    numbers[3] = 11.7;
-    numbers[4] = 13.7;
-    numbers[5] = 62.7;
-    numbers[6] = 11.2;
-    numbers[7] = 7.3;
-    numbers[8] = 4.5;
-
-    //quicksort(numbers, sizeof(float), 0, 8, cmp_float);
-    array_print_primitive(numbers, 9, "%.2f");
-    float v = -1.3;
-    printf("%d\n", binary_search_local_maxima(numbers, sizeof(float), 9, cmp_float));
 
     return 0;
 }
