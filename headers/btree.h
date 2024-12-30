40744ac89fa77454f982fc9c2171fd973dc7ae1b
azatshtru <adeveloper121@gmail.com>
2024-12-30 07:33:26 +0530

    Implemented insert for BTrees

--- headers/btree.h
+++ headers/btree.h
@@ -10,14 +10,17 @@ struct btree_node {
     int is_leaf;
 };
 
-struct btree {
+typedef struct btree {
     struct btree_node* root;
     int degree;
-};
+} BTree;
 
+BTree btree_new(int degree);
 int btree_init(struct btree* btree, int degree);
 int btree_node_init(struct btree_node* node_ptr, int is_leaf);
 struct btree_node* btree_search(struct btree btree, int key, struct btree_node* node);
 void btree_split_child(struct btree* btree, struct btree_node* x, int i);
+void btree_shush_insert(struct btree* btree, struct btree_node* x, int key);
+void btree_insert(struct btree* btree, int key);
 
 #endif
--- src/btree.c
+++ src/btree.c
@@ -1,7 +1,10 @@
 #include "../headers/btree.h"
-#include <string.h>
 
-typedef struct btree BTree;
+BTree btree_new(int degree) {
+    BTree btree;
+    btree_init(&btree, degree);
+    return btree;
+}
 
 int btree_init(BTree* btree, int degree) {
     struct btree_node* root = malloc(sizeof(struct btree_node));
@@ -36,12 +39,16 @@ struct btree_node* btree_search(BTree btree, int key, struct btree_node* node) {
 
 void btree_split_child(BTree* btree, struct btree_node* x, int i) {
     int t = btree->degree;
+    // y is a full child of x
     struct btree_node* y = *x->children + i;
 
+    // create a new node and add it to x's list of children
     struct btree_node z;
     btree_node_init(&z, y->is_leaf);
     vec_insert(x->children, i+1, z);
 
+
+    // insert the median of the full child y into x
     vec_insert(x->keys, i, vec_get(y->keys, t-1));
 
     // vec_resize((Vector*)z.keys, t-1);
@@ -49,6 +56,7 @@ void btree_split_child(BTree* btree, struct btree_node* x, int i) {
     //
     // vec_resize((Vector*)y->keys, t-1);
 
+    // split apart y's keys into y & z
     for(int j = t; j < 2*t-1; j++) {
         vec_push(z.keys, vec_get(y->keys, j)); 
     }
@@ -56,6 +64,7 @@ void btree_split_child(BTree* btree, struct btree_node* x, int i) {
         vec_zap(y->keys, -1, NULL); 
     }
 
+    // if y is not a leaf, we reassign y's children to y & z
     if(!y->is_leaf) {
         for(int j = t; j < 2*t; j++) {
             vec_push(z.children, vec_get(y->children, j)); 
@@ -63,5 +72,48 @@ void btree_split_child(BTree* btree, struct btree_node* x, int i) {
         for(int j = 0; j < t+1; j++) {
             vec_zap(y->children, -1, NULL); 
         }
+    }
+}
+
+void btree_shush_insert(BTree* btree, struct btree_node* x, int key) {
+    int t = btree->degree;
+    int i = vec_len(x->keys) - 1;
+
+    if(x->is_leaf) {
+        vec_push(x->keys, -1);
+        while(i >= 0 && key < vec_get(x->keys, i)) {
+            vec_set(x->keys, i+1, vec_get(x->keys, i));
+            --i;
+        }
+        vec_set(x->keys, i+1, key);
+    } else {
+        while(i >= 0 && key < vec_get(x->keys, i)) { --i; }
+        i++;
+        if(vec_len(vec_get(x->children, i).keys) == 2*t-1) {
+            btree_split_child(btree, x, i);
+            if(key > vec_get(x->keys, i)) {
+                i++;
+            }
+        }
+        btree_shush_insert(btree, *x->children + i, key);
+    }
+}
+
+void btree_insert(struct btree* btree, int key) {
+    int t = btree->degree;
+    struct btree_node* root = btree->root;
+
+    if(vec_len(root->keys) == 2*t-1) {
+        struct btree_node* new_root = malloc(sizeof(struct btree_node));
+
+        btree_node_init(new_root, 0);
+        btree->root = new_root;
+
+        vec_insert(new_root->children, 0, *root);
+
+        btree_split_child(btree, new_root, 0);
+        btree_shush_insert(btree, new_root, key);
+    } else {
+        btree_shush_insert(btree, root, key);
     }
 }
