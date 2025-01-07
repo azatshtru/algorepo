5f38e479ee65df91d0aaf26f9508238c10e28e64
azatshtru <adeveloper121@gmail.com>
2025-01-07 21:19:46 +0530

    Implemented Disjoint set for integers

---
+++ headers/disjoint_set_int.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#ifndef DISJOINT_SET_INT
+#define DISJOINT_SET_INT
+
+typedef struct disjoint_set_int {
+    int* parent;
+    int* size;
+} DisjointSetInt;
+
+DisjointSetInt disjoint_set_int_new(int max_size);
+void disjoint_set_int_free(DisjointSetInt* set);
+void disjoint_set_int_insert(DisjointSetInt* set, int v);
+int disjoint_set_int_find(DisjointSetInt* set, int v);
+void disjoint_set_int_union(DisjointSetInt* set, int a, int b);
+
+#endif
---
+++ src/disjoint_set_int.c
@@ -0,0 +1,39 @@
+#include "../headers/disjoint_set_int.h"
+
+DisjointSetInt disjoint_set_int_new(int max_size) {
+    DisjointSetInt set;
+    set.parent = malloc(sizeof(int) * max_size);
+    set.size = malloc(sizeof(int) * max_size);
+    return set;
+}
+
+void disjoint_set_int_free(DisjointSetInt* set) {
+    free(set->parent);
+    free(set->size);
+}
+
+void disjoint_set_int_insert(DisjointSetInt* set, int v) {
+    set->parent[v] = v;
+    set->size[v] = 1;
+}
+
+int disjoint_set_int_find(DisjointSetInt* set, int v) {
+    if(v == set->parent[v]) {
+        return v;
+    }
+    return set->parent[v] = disjoint_set_int_find(set, set->parent[v]);    
+}
+
+void disjoint_set_int_union(DisjointSetInt* set, int a, int b) {
+    a = disjoint_set_int_find(set, a);
+    b = disjoint_set_int_find(set, b);
+    if(a != b) {
+        if(set->size[a] < set->size[b]) {
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+        }
+        set->parent[b] = a;
+        set->size[a] += set->size[b];
+    }
+}
