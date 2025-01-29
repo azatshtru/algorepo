4860231a55cf49a6657f2bca4c70268b9eb4ab31
azatshtru <adeveloper121@gmail.com>
2025-01-30 00:18:47 +0530

    Implemented knapsack  01

---
+++ examples/knapsack_01.c
@@ -0,0 +1,28 @@
+#include "../headers/miniqutils.h"
+
+int knapsack_01(int* weights, int weights_len, int capacity) {
+    int knapsack[weights_len][capacity + 1];
+    memzero(knapsack, sizeof(int)*weights_len*(capacity + 1));
+    knapsack[0][0] = 1;
+    for (int k = 1; k <= capacity; k++) {
+        for (int x = 0; x <= weights_len; x++) {
+            if (x - weights[k] >= 0) knapsack[x][k] |= knapsack[x - weights[k]][k-1];
+            knapsack[x][k] |= knapsack[x][k-1];
+        }
+    }
+    return knapsack[weights_len - 1][capacity];
+}
+
+int knapsack_01x(int* weights, int weights_len, int capacity) {
+    int knapsack[capacity + 1];
+    memzero(knapsack, sizeof(int)*(capacity + 1));
+    knapsack[0] = 1;
+    for (int k = 1; k <= capacity; k++) {
+        for (int x = weights_len; x >= 0; x--) {
+            if (knapsack[x]) {
+                knapsack[x + weights[k]] = 1;
+            }
+        }
+    }
+    return knapsack[capacity];
+}
