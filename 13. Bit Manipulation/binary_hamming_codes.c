b93e0ff24b1abd6bcf106dee71ca58471ded8a64
azatshtru <adeveloper121@gmail.com>
2025-01-31 22:29:27 +0530

    Implemented counting_subgrids as an example for bit manipulation algorithms

--- 13. Bit Manipulation/binary_hamming_codes.c
+++
@@ -1,7 +0,0 @@
-#include "popcount.c"
-
-/// given two bit strings, find how often they differ
-
-int hamming(int a, int b) {
-    return parallel_popcount(a^b);
-}
--- examples/counting_subgrids.c
+++ examples/counting_subgrids.c
@@ -1,17 +1,21 @@
-#include "./popcount.c"
+#include "../headers/popcount.h"
 
 // in a given grid with white and black squares, find all the subgrids where the corners are black squares.
 // this can be solved easily if each row is represented as a binary number, then rows are anded.
 
-int count_subgrids(int n, int* grid) {
+int counting_subgrids(int n, int* grid) {
     int count = 0;
     for(int i = 0; i < n; i++) {
-        for(int j = i+1; j < n; j++) {
-            int k = parallel_popcount(grid[i]&grid[j]);
-            if(k>1) {
-                count+=(k-1);
+        for(int j = i + 1; j < n; j++) {
+            int k = popcount_parallel(grid[i] & grid[j]);
+            if(k > 1) {
+                count += k-1;
             }
         }
     }
     return count;
+}
+
+int main() {
+    return 0;
 }
